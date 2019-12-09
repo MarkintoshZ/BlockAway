0c7c861c1228dce4fc738f56f044033f205cce3f
Mark <bf-109g2@hotmail.com>
2019-12-09 13:57:15 -0500

    =Hackathon finished part

--- src/components/apps/adoption/AppAdoption.js
+++ src/components/apps/adoption/AppAdoption.js
@@ -1,21 +1,80 @@
 import React, { Component } from 'react';
 
+const childrenData = ['User0', 'User1', 'User2', 'User3']
+const adultsData = ['User0', 'User1', 'User2', 'User3']
+
 export class AppAdoption extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            showChild: false,
+            showAdult: false
         }
     }
 
+    showChildCallback = (e) => {
+        this.setState({
+            showChild: true,
+            showAdult: false
+        })
+    }
+
+    showAdultCallback = (e) => {
+        this.setState({
+            showChild: false,
+            showAdult: true
+        })
+    }
+
+    renderChildren() {
+        return (
+            <div>
+                <h2>Found Children</h2>
+                <ol>
+                    {childrenData.map((v) => {
+                        return <Users id={v} name={v} />
+                    })}
+                </ol>
+            </div>
+        )
+    }
+
+    renderAdults() {
+        return (
+            <div>
+                <h2>Found Adults</h2>
+                <ol>
+                    {adultsData.map((v) => {
+                        return <Users id={v} name={v} />
+                    })}
+                </ol>
+            </div>
+        )
+    }
+
     render() {
+        var { showAdult, showChild } = this.state; 
         return (
             <div style={{backgroundImage: '/AdoptionAppIcon.jpg'}}>
                 <section style={{display: 'initial'}}>
-                    <button className='btn-primary'>Find Parents</button>
-                    <button className='btn-primary'>Find Children</button>
+                    <button className='btn-primary' onClick={this.showAdultCallback} >Find Parents</button>
+                    <button className='btn-primary' onClick={this.showChildCallback} >Find Children</button>
                 </section>
+
+                {(showAdult)? this.renderAdults() : null}
+                {(showChild)? this.renderChildren() : null}
             </div>
+        )
+    }
+}
+
+class Users extends Component {
+    render() {
+        const { id, name } = this.props
+        return (
+            <li id={id}>
+                <button className='btn-primary' style={{width: '100px', borderRadius: '10px', margin: '10px'}}>{name}</button>
+            </li>
         )
     }
 }
